a5431f1d0f65a2cfd1e8b202fc18a2d9768d666c
Vildan Softic <zewa666@gmail.com>
2019-12-17 22:55:05 +0100

    feat(app): finished raw board with both circles and fortress ground

--- src/board/board.ts
+++ src/board/board.ts
@@ -1,49 +1,60 @@
 import { computedFrom } from "aurelia-framework";
 
-import { Tile } from "./tile";
+import { Tile, TilePlacement, TileRing } from "./tile";
 
 export class Board {
   private tiles: Tile[] = [
     ...Array.from<Tile, Tile>(Array(11), (_, idx) => ({
       type: "wood",
       placement: "bottom",
-      isCorner: idx === 0 || idx === 10
+      isCorner: idx === 0 || idx === 10,
+      ring: "outer"
     })),
     ...Array.from<Tile, Tile>(Array(9), () => ({
-      type: "stone",
+      type: "food",
       placement: "left",
-      isCorner: false
+      isCorner: false,
+      ring: "outer"
     })),
     ...Array.from<Tile, Tile>(Array(11), (_, idx) => ({
-      type: "iron",
+      type: "stone",
       placement: "top",
-      isCorner: idx === 0 || idx === 10
+      isCorner: idx === 0 || idx === 10,
+      ring: "outer"
     })),
     ...Array.from<Tile, Tile>(Array(9), () => ({
       type: "gold",
       placement: "right",
-      isCorner: false
+      isCorner: false,
+      ring: "outer"
+    })),
+    ...Array.from<Tile, Tile>(Array(11), (_, idx) => ({
+      type: "iron",
+      placement: "bottom",
+      isCorner: idx === 0 || idx === 10,
+      ring: "inner"
+    })),
+    ...Array.from<Tile, Tile>(Array(9), () => ({
+      type: "mana",
+      placement: "left",
+      isCorner: false,
+      ring: "inner"
+    })),
+    ...Array.from<Tile, Tile>(Array(11), (_, idx) => ({
+      type: "coal",
+      placement: "top",
+      isCorner: idx === 0 || idx === 10,
+      ring: "inner"
+    })),
+    ...Array.from<Tile, Tile>(Array(9), () => ({
+      type: "blood",
+      placement: "right",
+      isCorner: false,
+      ring: "inner"
     })),
   ];
 
-  @computedFrom("tiles")
-  public get tilesTop() {
-    return this.tiles.filter(t => t.placement === "top");
+  public getTiles(ring: TileRing, placement: TilePlacement) {
+    return this.tiles.filter(t => t.placement === placement && t.ring === ring);
   }
-
-  @computedFrom("tiles")
-  public get tilesRight() {
-    return this.tiles.filter(t => t.placement === "right");
-  }
-
-  @computedFrom("tiles")
-  public get tilesBottom() {
-    return this.tiles.filter(t => t.placement === "bottom");
-  }
-
-  @computedFrom("tiles")
-  public get tilesLeft() {
-    return this.tiles.filter(t => t.placement === "left");
-  }
-
 }
--- src/board/tile.ts
+++ src/board/tile.ts
@@ -1,10 +1,12 @@
 import { bindable } from "aurelia-framework";
 
-export type TileType = "wood" | "stone" | "iron" | "gold"; 
+export type TileType = "wood" | "stone" | "food" | "gold" | "iron" | "coal" | "mana" | "blood"; 
 export type TilePlacement = "bottom" | "left" | "top" | "right";
+export type TileRing = "inner" | "outer";
 
 export class Tile {
   @bindable public type: TileType;
   @bindable public placement: TilePlacement;
   @bindable public isCorner: boolean = false;
+  @bindable public ring: "inner" | "outer";
 }
