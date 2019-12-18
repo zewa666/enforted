f86b09cf750db21516de392f200f326783d1eb8e
Vildan Softic <vsoftic@ranorex.com>
2019-12-18 10:43:22 +0100

    chore(app): add css sourcemaps

--- aurelia_project/tasks/process-css.ts
+++ aurelia_project/tasks/process-css.ts
@@ -1,11 +1,15 @@
-import {build} from 'aurelia-cli';
+import { build } from 'aurelia-cli';
 import * as gulp from 'gulp';
 import * as project from '../aurelia.json';
 import * as sass from 'gulp-sass';
 
+const sourcemaps = require('gulp-sourcemaps');
+
 export default function processCSS() {
-  return gulp.src(project.cssProcessor.source, {sourcemaps: true})
+  return gulp.src(project.cssProcessor.source, { sourcemaps: true })
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
     .pipe(build.bundle());
 }
 
