2ccfb67fa59245bf8bf6a18ba5b2f1439e93f759
Vildan Softic <zewa666@gmail.com>
2019-12-21 21:07:00 +0100

    chore(app): add stylelint linter

--- aurelia_project/tasks/lint.ts
+++ aurelia_project/tasks/lint.ts
@@ -1,12 +1,24 @@
-import * as gulp from 'gulp';
-import tslint from 'gulp-tslint';
-import * as project from '../aurelia.json';
+import * as gulp from "gulp";
+import * as gulpStylelint from "gulp-stylelint";
+import tslint from "gulp-tslint";
+import * as merge from "merge-stream";
+
+import * as project from "../aurelia.json";
 
 export default function lint() {
-  return gulp.src(project.transpiler.source)
+  const tsLinting = gulp.src(project.transpiler.source)
     .pipe(tslint({
+      formatter: "stylish",
       tslint: require("tslint"),
-      formatter: 'stylish'
     }))
-    .pipe(tslint.report({summarizeFailureOutput: true}));
+    .pipe(tslint.report({ summarizeFailureOutput: true }));
+
+  const scssLinting = gulp.src(project.cssProcessor.source)
+    .pipe(gulpStylelint({
+      reporters: [
+        { formatter: "string", console: true }
+      ]
+    }));
+
+  return merge(tsLinting, scssLinting);
 }
