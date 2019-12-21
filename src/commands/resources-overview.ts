0ccf871701c6a995b28b338b240dba3b46054c8c
Vildan Softic <zewa666@gmail.com>
2019-12-21 21:18:50 +0100

    refactor(app): dynamic resource listing

--- src/commands/resources-overview.ts
+++ src/commands/resources-overview.ts
@@ -1,7 +1,25 @@
+import { computedFrom } from "aurelia-framework";
 import { connectTo } from "aurelia-store";
 import { pluck } from "rxjs/operators";
 
+import { Resources, ResourcesIcons } from "../store/state";
+import { ucFirst } from "../utils/utils";
+
 @connectTo((store) => store.state.pipe(pluck("resources")))
 export class ResourcesOverview {
+  public state: Resources;
 
+  @computedFrom("state")
+  public get resources() {
+    if (!this.state) {
+      return [];
+    }
+
+    return Object.entries(this.state)
+      .map((e) => ({
+        icon: ResourcesIcons[e[0]],
+        name: ucFirst(e[0]),
+        value: e[1],
+      }));
+  }
 }
---
+++ src/utils/utils.ts
@@ -0,0 +1,3 @@
+export function ucFirst(s: string) {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
