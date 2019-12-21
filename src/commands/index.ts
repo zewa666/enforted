802ad24025056dea3566c9e8e27fc53dae15c1f8
Vildan Softic <zewa666@gmail.com>
2019-12-21 18:37:38 +0100

    refactor(app): resources -> resources-overview

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,12 +2,12 @@ import { FrameworkConfiguration } from "aurelia-framework";
 
 import { CommandBar } from "./command-bar";
 import { Dice } from "./dice";
-import { Resources } from "./resources";
+import { ResourcesOverview } from "./resources-overview";
 
 export function configure(config: FrameworkConfiguration) {
   config.globalResources([
     CommandBar,
     Dice,
-    Resources
+    ResourcesOverview
   ]);
 }
--- src/commands/resources-overview.ts
+++ src/commands/resources-overview.ts
@@ -2,6 +2,6 @@ import { connectTo } from "aurelia-store";
 import { pluck } from "rxjs/operators";
 
 @connectTo((store) => store.state.pipe(pluck("resources")))
-export class Resources {
+export class ResourcesOverview {
 
 }
