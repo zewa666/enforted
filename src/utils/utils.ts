d83e1a33cfd5060b672a54e8356f93d0d3bc892f
Vildan Softic <zewa666@gmail.com>
2019-12-22 14:47:39 +0100

    feat(app): added generic dialog with helper

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,22 @@
+import { DialogService } from "aurelia-dialog";
+import { Container } from "aurelia-framework";
+
 export function ucFirst(s: string) {
   return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+export interface DialogModel {
+  [key: string]: any;
+  view: string;
+}
+
+export function openDialog(viewModel: any, model: DialogModel) {
+  const dialogService = Container.instance.get(DialogService);
+
+  return dialogService.open({
+    centerHorizontalOnly: true,
+    model,
+    view: "utils/generic-dialog.html",
+    viewModel,
+  });
 }
