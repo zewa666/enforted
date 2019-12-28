001dcda9ea016f29bd5231a702908e0dc57ec8a2
Vildan Softic <zewa666@gmail.com>
2019-12-28 14:44:06 +0100

    chore(app): uninstall gulp-notify

--- aurelia_project/tasks/transpile.ts
+++ aurelia_project/tasks/transpile.ts
@@ -1,30 +1,29 @@
-import * as gulp from 'gulp';
-import * as plumber from 'gulp-plumber';
-import * as notify from 'gulp-notify';
-import * as rename from 'gulp-rename';
-import * as ts from 'gulp-typescript';
-import * as project from '../aurelia.json';
-import * as fs from 'fs';
-import * as through from 'through2';
-import { CLIOptions, build, Configuration } from 'aurelia-cli';
+import { build, CLIOptions, Configuration } from "aurelia-cli";
+import * as fs from "fs";
+import * as gulp from "gulp";
+import * as plumber from "gulp-plumber";
+import * as rename from "gulp-rename";
+import * as ts from "gulp-typescript";
+import * as through from "through2";
+import * as project from "../aurelia.json";
 
 function configureEnvironment() {
-  let env = CLIOptions.getEnvironment();
+  const env = CLIOptions.getEnvironment();
 
   return gulp.src(`aurelia_project/environments/${env}.ts`, { since: gulp.lastRun(configureEnvironment) })
-    .pipe(rename('environment.ts'))
-    .pipe(through.obj(function (file, _, cb) {
+    .pipe(rename("environment.ts"))
+    .pipe(through.obj(function(file, _, cb) {
       // https://github.com/aurelia/cli/issues/1031
-      fs.unlink(`${project.paths.root}/${file.relative}`, function () { cb(null, file); });
+      fs.unlink(`${project.paths.root}/${file.relative}`, function() { cb(null, file); });
     }))
     .pipe(gulp.dest(project.paths.root));
 }
 
-var typescriptCompiler = typescriptCompiler || null;
+let typescriptCompiler = typescriptCompiler || null;
 
 function buildTypeScript() {
-  typescriptCompiler = ts.createProject('tsconfig.json', {
-    typescript: require('typescript')
+  typescriptCompiler = ts.createProject("tsconfig.json", {
+    typescript: require("typescript")
   });
 
   return gulp.src(project.transpiler.dtsSource)
@@ -32,7 +31,8 @@ function buildTypeScript() {
       sourcemaps: true,
       since: gulp.lastRun(buildTypeScript)
     }))
-    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+    // tslint:disable-next-line:no-console
+    .pipe(plumber({ errorHandler: (error) => console.error(error.message) }))
     .pipe(typescriptCompiler())
     .pipe(build.bundle());
 }
@@ -41,4 +41,3 @@ export default gulp.series(
   configureEnvironment,
   buildTypeScript
 );
-
