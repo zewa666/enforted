5494393eda91130707759d187882ffbdf17d81f1
Vildan Softic <zewa666@gmail.com>
2020-01-04 21:47:55 +0100

    chore(app): element generator updated

--- aurelia_project/generators/element.ts
+++ aurelia_project/generators/element.ts
@@ -1,44 +1,46 @@
-import {inject} from 'aurelia-dependency-injection';
-import {Project, ProjectItem, CLIOptions, UI} from 'aurelia-cli';
+import { CLIOptions, Project, ProjectItem, UI } from "aurelia-cli";
+import { inject } from "aurelia-dependency-injection";
+import { kebabCase } from "lodash";
 
 @inject(Project, CLIOptions, UI)
 export default class ElementGenerator {
   constructor(private project: Project, private options: CLIOptions, private ui: UI) { }
 
-  async execute() {
+  public async execute() {
     const name = await this.ui.ensureAnswer(
       this.options.args[0],
-      'What would you like to call the custom element?'
+      "What would you like to call the custom element?"
     );
 
-    let fileName = this.project.makeFileName(name);
-    let className = this.project.makeClassName(name);
+    const fileName = this.project.makeFileName(name);
+    const className = this.project.makeClassName(name);
 
     this.project.elements.add(
       ProjectItem.text(`${fileName}.ts`, this.generateJSSource(className)),
-      ProjectItem.text(`${fileName}.html`, this.generateHTMLSource(className))
+      ProjectItem.text(`${fileName}.html`, this.generateHTMLSource(className)),
+      ProjectItem.text(`_${fileName}.scss`, `.${kebabCase(className)} {}`)
     );
 
     await this.project.commitChanges();
     await this.ui.log(`Created ${fileName}.`);
   }
 
-  generateJSSource(className) {
-    return `import {bindable} from 'aurelia-framework';
+  public generateJSSource(className) {
+    return `import { autoinject } from "aurelia-framework";
+import { Store } from "aurelia-store";
 
+import { State } from "../store/state";
+
+@autoinject()
 export class ${className} {
-  @bindable value;
-
-  valueChanged(newValue, oldValue) {
-    //
-  }
+  constructor(private store: Store<State>)
 }
 `;
   }
 
-  generateHTMLSource(className) {
-    return `<template>
-  <h1>\${value}</h1>
+  public generateHTMLSource(className) {
+    return `<template class="${kebabCase(className)}">
+
 </template>
 `;
   }
