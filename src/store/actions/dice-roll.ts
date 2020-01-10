f4fec5b9e517f4d79b04e72770b2da880067149b
Vildan Softic <zewa666@gmail.com>
2020-01-10 12:52:48 +0100

    refactor(actions): extract dice and round specific actions

---
+++ src/store/actions/dice-roll.ts
@@ -0,0 +1,105 @@
+import { AvailableTragedyEvents } from "../../board/tragedy";
+import { AvailableTileBuildings } from "../../buildings/tile-building";
+import { Player } from "../../player/player";
+import { Resources } from "../../resources/index";
+import { randBetween } from "../helper";
+import { State } from "../index";
+
+export function rollDice(state: State, diceOverload?: number): State {
+  const isStumblingStep = state.activeTragedy === AvailableTragedyEvents.StumblingSteps;
+  const idxOfTile = state.tiles.findIndex((t) => t.id === state.players[0].currentTileId);
+  const roll = diceOverload || (isStumblingStep ? 1 : randBetween(1, 6));
+  const newPosition = idxOfTile + roll;
+  const isNextRound = newPosition > state.tiles.length - 1;
+  const newStumblingSteps = (state.activeTragedyParams === undefined || state.activeTragedyParams[0] === 1)
+    ? undefined
+    : [state.activeTragedyParams[0] - 1];
+  return {
+    ...state,
+    activeFortressBuildingConstruction: isNextRound ? undefined : state.activeFortressBuildingConstruction,
+    activeTragedy: isNextRound
+      ? undefined
+      : isStumblingStep && newStumblingSteps === undefined
+        ? undefined
+        : state.activeTragedy,
+    activeTragedyParams: isNextRound
+      ? undefined
+      : isStumblingStep
+        ? newStumblingSteps
+        : state.activeTragedyParams,
+    lastDiceRoll: roll,
+    players: [
+      {
+        ...state.players[0],
+        currentTileId: newPosition > state.tiles.length - 1
+          ? state.tiles[Math.abs(newPosition - state.tiles.length)].id
+          : state.tiles[newPosition].id
+      } as Player
+    ],
+    purchaseInProgress: undefined,
+    resources: isNextRound
+      ? gatherResources(state).resources
+      : state.resources,
+    round: isNextRound
+      ? state.round + 1
+      : state.round,
+  };
+}
+
+export const PRODUCED_RESOURCES_PER_ROUND = 3;
+export function gatherResources(state: State): State {
+  const shrines = state.tileBuildings
+    .filter((b) => b.type === "shrine");
+  const resourcesProduced = PRODUCED_RESOURCES_PER_ROUND
+    + (shrines.length === 4
+      ? 1
+      : 0);
+  const resources = state.tileBuildings
+    .filter((b) => b.type !== "shrine")
+    .filter((b) => {
+      switch (state.activeTragedy) {
+        case AvailableTragedyEvents.BurningTrees:
+          return b.type !== "sawmill";
+        case AvailableTragedyEvents.ContaminatedBlood:
+          return b.type !== "butchery";
+        case AvailableTragedyEvents.Rockfall:
+          return b.type !== "quarry";
+        case AvailableTragedyEvents.ShatteringEarth:
+          return b.type !== "mana_rift";
+        case AvailableTragedyEvents.Vermins:
+          return b.type !== "farm";
+        case AvailableTragedyEvents.CollapsedMines:
+          const mineType = state.activeTragedyParams[0] as AvailableTileBuildings;
+          return b.type !== mineType;
+        default:
+          return true;
+      }
+    })
+    .reduce((prev, curr) => {
+      prev[state.tiles
+        .find((t) => t.id === curr.tileId)
+        .type] += resourcesProduced;
+      return prev;
+    }, Object.assign({}, state.resources) as Resources);
+  return gatherShrineResources({
+    ...state,
+    resources
+  });
+}
+
+export function gatherShrineResources(state: State) {
+  const shrines = state.tileBuildings
+    .filter((b) => b.type === "shrine");
+  if (!shrines) {
+    return state;
+  }
+  return {
+    ...state,
+    resources: {
+      ...state.resources,
+      blood: shrines.length > 2 ? state.resources.blood + 1 : state.resources.blood,
+      gold: shrines.length > 0 ? state.resources.gold + 1 : state.resources.gold,
+      mana: shrines.length > 1 ? state.resources.mana + 1 : state.resources.mana,
+    }
+  };
+}
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,2 +1,3 @@
 export * from "./state";
 export * from "./actions/commands";
+export * from "./actions/dice-roll";
