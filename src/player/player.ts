ec39fc3063740c74565f8c0b9f26665764dc951b
Vildan Softic <zewa666@gmail.com>
2020-01-12 08:23:58 +0100

    refactor(player): remove unnecessary binding
    https://github.com/zewa666/enforted/issues/14

--- src/player/player.ts
+++ src/player/player.ts
@@ -1,19 +1,6 @@
-import { autoinject, bindable } from "aurelia-framework";
-import { Store } from "aurelia-store";
+import { bindable } from "aurelia-framework";
 
-import { Tile } from "../board/tile";
-import { State } from "../store/state";
-
-@autoinject()
 export class Player {
   @bindable public currentTileId: string;
   @bindable public name: string;
-
-  private currentTile: Tile = {} as Tile;
-
-  constructor(private store: Store<State>) { }
-
-  public currentTileIdChanged() {
-    this.currentTile = (this.store as any)._state.getValue();
-  }
 }
