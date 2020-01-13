2d2c25dc0c9f425f0f51c84a031b5129ba23124c
Vildan Softic <vsoftic@ranorex.com>
2020-01-13 08:27:23 +0100

    chore(animation): added finished polyfill for chrome

--- src/animator/animator.ts
+++ src/animator/animator.ts
@@ -96,3 +96,28 @@ export class WebAnimationAnimator extends Animator {
     return Promise.resolve(false);
   }
 }
+
+// only polyfill .finished in browsers that already support animate()
+if (document.body.animate) {
+
+  // Chrome does not seem to expose the Animation constructor globally
+  if (typeof Animation === "undefined") {
+    window.Animation = (document.body.animate as any)({}).constructor;
+  }
+
+  if (Animation.prototype.finished === undefined) {
+    Object.defineProperty(Animation.prototype, "finished", {
+      get() {
+        if (!this._finished) {
+          this._finished = this.playState === "finished" ?
+            Promise.resolve() :
+            new Promise((resolve, reject) => {
+              this.addEventListener("finish", resolve, {once: true});
+              this.addEventListener("cancel", reject, {once: true});
+            });
+        }
+        return this._finished;
+      }
+    });
+  }
+}
